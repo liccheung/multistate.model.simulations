cdd7b4cad13c462ff700c6da904ebf67ba2bacd3
Li C. Cheung <li.cheung@nih.gov>
2022-06-02 11:44:14 -0400

    Update BJC_simulation25.75.R

--- BJC_simulation25.75.R
+++ BJC_simulation25.75.R
@@ -45,9 +45,11 @@ t5 <- rgamma(num,shape=a5,rate=exp(b51)*exp(b52*x)) #time from clinical cancer t
 #set to ~25% overall deaths after 15 years according to LYFS-CT mortality model for smokers, age 40-84
 t_death <- ifelse(t3<=t1,t3,
                   ifelse(((t1+t4)<=(t1+t2)),(t1+t4),(t1+t2+t5)))
-#14% with cancer before death
+table(t_death<=15)  
+#7% with cancer before death
 t_can <- ifelse((t1<t3) & ((t1+t2)<(t1+t4)),(t1+t2),NA)
 t_precan <- ifelse(t1<t3,t1,NA)
+table(is.na(t_can)==0 & t_can<=15)  
  
 v0 <- 0 #runif(num,min=0,max=25) #enrollment date and first screen for screening arm
 n_visits <- sample.int(15, num, replace= TRUE)-1 #15 #uncomment for no dropout
